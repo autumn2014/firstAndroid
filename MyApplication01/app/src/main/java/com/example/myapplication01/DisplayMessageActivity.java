729e45b46de7bb8235fb12a7a2c861048b285cef
code-pig <283139246@qq.com>
2019-05-21 08:55:00 +0800

    reverse

--- MyApplication01/app/src/main/java/com/example/myapplication01/DisplayMessageActivity.java
+++ MyApplication01/app/src/main/java/com/example/myapplication01/DisplayMessageActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+        TextView textView=findViewById(R.id.textView02);
+        textView.setText(R.string.app_name);
     }
 }
